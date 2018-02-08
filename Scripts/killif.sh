8d60c9ba84ad60c89ea1f7c087e89f0c5b2a5212
znxftw <vishnu2101@gmail.com>
2018-02-08 11:22:21 +0530

    Program to kill processes on startup

--- Scripts/killif.sh
+++ Scripts/killif.sh
@@ -2,12 +2,12 @@
 if [ $# -ne 1 ];
 then
 	echo "Invalid number of arguments";
-	exit 0
+	exit 0;
 fi
 while [ true ];
 do
 	USAGE=$(ps -eo pid -o pcpu -o command --no-headers | sort -nrk 2 | head -n 1 | cut -d "." -f1 |  awk '{print $2}' )
-	if [ $USAGE -gt 10 ]
+	if [ $USAGE -gt $1 ]
 	then
 		MOSTUSAGE=$(ps -eo pid -o pcpu -o command --no-headers | sort -nrk 2 | head -n 1 | awk '{print $1}' )
 		kill -9 $MOSTUSAGE
